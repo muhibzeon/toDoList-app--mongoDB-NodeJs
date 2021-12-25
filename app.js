9e4382ed2b8f155d8bf27be2e4671f903477998c
Mohammad Muhib <mohammadmuhib@Mohammads-MacBook-Air.local>
2021-12-25 20:39:43 +0100

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -144,6 +144,11 @@ app.post("/delete", function(req,res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
 
@@ -151,6 +156,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started on port 3000");
 });
